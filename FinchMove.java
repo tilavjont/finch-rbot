52284fd76d9209576d4b1d47ad4551f2d65693b3
tilavjont <tilavjontolibov@icloud.com>
2025-05-29 11:22:29 -0400

    Update FinchMove.java

--- FinchMove.java
+++ FinchMove.java
@@ -1,1 +1,18 @@
+public class FinchMove {
+	public static void main(String[] args) {
+      Finch m = new Finch();
+      
+      while(m.getDistance() < 5) {
+          int note = (int)(Math.Random*104)+32;
+        	int beat = (int)(Math.Random*17);
+        	m.setBeak(162, 0, 255);
+        	m.playNote(note, beat);
+            m.setMotors(50, 50);
+      }
+      
 
+      
+      m.stopAll();
+      m.disconnect();
+}
+}
